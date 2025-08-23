5d90bc144980473ef11961b0def3b8c199898983
usha1116 <ushasahu1116@gmail.com>
2025-08-24 00:59:30 +0530

    final

--- client-app/url-shortner/src/Container/Container.tsx
+++ client-app/url-shortner/src/Container/Container.tsx
@@ -1,13 +1,36 @@
 import * as React from 'react';
 import FormContainer from '../FormContainer/FormConatainer';
+import type { UrlData } from '../interface/UrlData';
+import axios from 'axios';
+import { serverUrl } from '../helpers/Constants';
+import DataTable from '../components/DataTable/DataTable';
 
 interface IContainerProps {
 }
 
 const Container: React.FunctionComponent<IContainerProps> = () => {
+  const [data ,setData] = React.useState<UrlData[]>([]);
+const [reload , setReload] = React.useState<boolean>(false);
+
+const updateReloadState = ():void =>{
+    setReload(true);
+}
+
+  const fetchTableData = async()=>{
+      const response = await axios.get(`${serverUrl}/shortUrl`)
+      console.log("The response is:", response.data);
+      setData(response.data);
+      //console.log("Data :" , data)
+      setReload(false);
+  }
+  React.useEffect(() => {
+      fetchTableData();
+  }, [reload])
+
   return(
     <>
-        <FormContainer/>
+        <FormContainer updateReloadState={updateReloadState}/>
+        <DataTable updateReloadState={updateReloadState} data={data}/>
     </>
   ) ;
 };
